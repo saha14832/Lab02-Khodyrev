72fac056dafa200522260e1ce5a95faecb25b115
saha14832 <alex14832@mail.ru>
2026-09-11 12:57:07 +0300

    Lab02: var keyword

--- obj/Debug/net10.0/Lab02-Khodyrev.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Khodyrev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Khodyrev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895c8ed0a605216b5ebffe22cfaab00d4a0eb438")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b949e94b292df61a83247e4c080855633fdeedea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Khodyrev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Khodyrev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
